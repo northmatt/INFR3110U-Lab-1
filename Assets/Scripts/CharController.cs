b6a37649a4f1eef227824252e19a8b2fa32f1c21
northmatt <33724702+northmatt@users.noreply.github.com>
2022-09-27 14:04:11 -0400

    Ground Detection
    -Need to compare CPU time to Physics.CheckSphere

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharController : MonoBehaviour {
-    public float moveForce;
-    public float jumpForce;
+    public float moveForce = 0f;
+    public float jumpForce = 0f;
+    public float groundedDot = 0f;
 
     private Rigidbody rBody;
-    private List<Collision> collisions = new List<Collision>();
-    private Vector3 inputs = Vector3.zero;
+    private List<GameObject> groundedObjects = new List<GameObject>();
+    private Vector2 inputs = Vector2.zero;
+    public byte jump = 0;
+    private bool grounded = false;
 
     // Start is called before the first frame update
     void Start() {
@@ -19,49 +22,60 @@ public class CharController : MonoBehaviour {
     void Update() {
         inputs.Set(
             Input.GetAxisRaw("Horizontal"),
-            Input.GetButtonDown("Jump") ? 1f : inputs.y,
             Input.GetAxisRaw("Vertical")
         );
+
+        if (Input.GetButton("Jump") && jump == 0 && grounded)
+            jump = 4;
     }
 
     private void FixedUpdate() {
-        bool grounded = IsGrounded();
+        grounded = groundedObjects.Count > 0;
 
-        rBody.AddForce(inputs.x * moveForce, 0f, inputs.z * moveForce);
-
-        if (grounded && inputs.y > 0.01f)
-            rBody.AddForce(0f, inputs.y * jumpForce, 0f, ForceMode.VelocityChange);
-
+        rBody.AddForce(inputs.x * moveForce, 0f, inputs.y * moveForce);
         inputs = Vector3.zero;
+
+        if (jump == 4)
+            rBody.AddForce(0f, jumpForce, 0f, ForceMode.VelocityChange);
+
+        //reduce jump cooldown if grounded
+        if (jump > 0 && grounded)
+            --jump;
     }
 
-    //loop through all collisions and get contact normal range
-    private bool IsGrounded() {
+    private void OnCollisionEnter(Collision collision) {
         //upto 64 contact points, unlikely to ever need >20. List has dynamic scaling
         //List<ContactPoint> contactPoints = new List<ContactPoint>();
         ContactPoint[] contactPoints = new ContactPoint[20];
         int contactPointsCount = 0;
+        contactPointsCount = collision.GetContacts(contactPoints);
 
-        foreach (Collision curCollision in collisions) {
-            contactPointsCount = curCollision.GetContacts(contactPoints);
-
-            for (int i = 0; i < contactPointsCount; ++i){
-                //Debug.Log("Normal: " + contactPoints[i].normal);
+        //loop through all contacts and compare contact normal to a specific range
+        for (int i = 0; i < contactPointsCount; ++i)
+            if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > groundedDot) {
+                groundedObjects.Add(collision.gameObject);
+                break;
             }
-        }
-
-        return true;
     }
 
-    private void OnCollisionEnter(Collision collision) {
-        collisions.Add(collision);
-        Debug.Log("Enter: " + collision.GetHashCode());
-        foreach (Collision curCollision in collisions) Debug.Log(curCollision.gameObject.name);
+    private void OnCollisionStay(Collision collision) {
+        //upto 64 contact points, unlikely to ever need >20. List has dynamic scaling
+        //List<ContactPoint> contactPoints = new List<ContactPoint>();
+        ContactPoint[] contactPoints = new ContactPoint[20];
+        int contactPointsCount = 0;
+        contactPointsCount = collision.GetContacts(contactPoints);
+
+        //loop through all contacts and compare contact normal to a specific range
+        for (int i = 0; i < contactPointsCount; ++i)
+            if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > groundedDot) {
+                if (!groundedObjects.Contains(collision.gameObject)) groundedObjects.Add(collision.gameObject);
+                return;
+            }
+
+        groundedObjects.Remove(collision.gameObject);
     }
 
     private void OnCollisionExit(Collision collision) {
-        collisions.Remove(collision);
-        Debug.Log("Exit" + collision.GetHashCode());
-        foreach (Collision curCollision in collisions) Debug.Log(curCollision.gameObject.name);
+        groundedObjects.Remove(collision.gameObject);
     }
 }
